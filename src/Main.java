104c597beb89c19459b1df30ae3b1e2b457f5cdd
danibarrosj <danibarrosj@outlook.com>
2025-04-08 10:28:59 -0400

    Converts Nautical Miles to KM and Miles

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        final double KILOMETERS_PER_NAUTICAL_MILE = 1.852;
-        final double MILES_PER_NAUTICAL_MILE = 1.150779;
+        final double km_PER_nm = 1.852;
+        final double mi_PER_nm = 1.150779;
 
         double nauticalMiles = 10.0; // example value
 
-        double kilometers = nauticalMiles * KILOMETERS_PER_NAUTICAL_MILE;
-        double miles = nauticalMiles * MILES_PER_NAUTICAL_MILE;
+        double kilometers = nauticalMiles * km_PER_nm;
+        double miles = nauticalMiles * mi_PER_nm;
 
         System.out.println(nauticalMiles + " nautical miles is equal to "
                 + kilometers + " kilometers and "
